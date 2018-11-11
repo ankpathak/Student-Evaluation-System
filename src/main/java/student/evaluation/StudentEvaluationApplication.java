24e3f6b6739a7d1b00c390762663d4b7cf62a8da
Ankit Pathak <ankpathak595@gmail.com>
2018-11-11 15:22:21 +0530

    Project Initialized

---
+++ src/main/java/student/evaluation/StudentEvaluationApplication.java
@@ -0,0 +1,13 @@
+package student.evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentEvaluationApplication.class, args);
+		System.out.println("Hello");
+	}
+}
